c25c0591a0f2210dc04d34d9fb73ae8fb1c23035
Bartek Borowczyk <bartek@websamuraj.pl>
2019-03-02 11:41:42 +0100

    Update packega.json and App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Aplikacja React
+            Aplikacja React na Github Pages
           </p>
         </header>
       </div>
