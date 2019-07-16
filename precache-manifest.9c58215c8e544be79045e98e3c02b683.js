0ae41e7ee9599d4a34466d0dfcfd7782d8f20bbe
Paweł Jarosz <jarosz.paw@gmail.com>
2019-07-16 11:25:50 +0200

    Updates

---
+++ precache-manifest.9c58215c8e544be79045e98e3c02b683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/AppReact/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b0cebed97631ddd20540",
+    "url": "/AppReact/static/js/runtime~main.b0cebed9.js"
+  },
+  {
+    "revision": "e5f1a940a15c4fbf2ab5",
+    "url": "/AppReact/static/js/main.e5f1a940.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/AppReact/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e5f1a940a15c4fbf2ab5",
+    "url": "/AppReact/static/css/main.84ebed30.chunk.css"
+  },
+  {
+    "revision": "9aebb40960bf8b3ca7da2e62697afa2a",
+    "url": "/AppReact/index.html"
+  }
+];
